9183b8489771000202bdde57d55bb61f0c9e82ab
Ruhul Amin <150836543+ruhulamin-et15@users.noreply.github.com>
2023-11-15 18:52:52 +0600

    upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+// import { FaBeer } from 'react-icons/fa';
+//import Button from 'react-bootstrap/Button';
+// import { createRoot } from 'react-dom/client';
+// import { uuid } from 'uuidv4';
+// import Home from "./Home";
+import Index from "./routes";
+
+function App() {
+  return (
+    <div>
+      <Index/>
+    </div>
+  );
+}
+
+export default App;
